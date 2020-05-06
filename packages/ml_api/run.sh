05a012cc9e5cda12717cc4f917916b62e64651fa
Ali Naji <ali-naji@users.noreply.github.com>
2020-05-06 13:27:06 -0700

    increased timeout

--- packages/ml_api/run.sh
+++ packages/ml_api/run.sh
@@ -1,2 +1,2 @@
 export IS_DEBUG=${DEBUG:-false}
-exec gunicorn --bind 0.0.0.0:5000 --log-level debug --access-logfile - --error-logfile - run:application --timeout 120 --workers 3
+exec gunicorn --bind 0.0.0.0:5000 --log-level debug --access-logfile - --error-logfile - run:application --timeout 120
